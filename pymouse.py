680f6064e36f1165ecb2e740b67865b1236923a6
pepijndevos <pepijndevos@c5de4e7c-6ed8-11de-921c-dbe4921c5040>
2009-09-26 14:28:23 +0000

    Forgot to add the @abstractmethod to the base class. Now windows is broken until I find a way to get the screen size

--- pymouse.py
+++ pymouse.py
@@ -1,15 +1,17 @@
 # -*- coding: iso-8859-1 -*-
 import sys
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 class PyMouseMeta:
 	__metaclass__ = ABCMeta
 	
+	@abstractmethod
 	def press(self, x, y, button = 0):
 		"""Press the mouse on a givven x, y and button.
 		Button is defined as 0 = left, 1 = middle, 2 = right."""
 		pass
 	
+	@abstractmethod
 	def release(self, x, y):
 		"""Release all mouse buttons"""
 		pass
@@ -20,15 +22,18 @@ class PyMouseMeta:
 		self.press(x, y, button)
 		self.release(x, y, button)
 
+	@abstractmethod
 	def move(self, x, y):
 		"""Move the mouse to a givven x and y"""
 		pass
 
+	@abstractmethod
 	def position(self):
 		"""Get the current mouse position in pixels.
 		Returns a tuple of 2 integers"""
 		pass
 
+	@abstractmethod
 	def screen_size(self):
 		"""Get the current screen size in pixels.
 		Returns a tuple of 2 integers"""
