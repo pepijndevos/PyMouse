4da1d10be2ebb9dd2d9a70c877c9435af3c256d2
Pepijn de Vos <pepijndevos@pepijn-de-voss-imac.local>
2010-01-10 16:13:59 +0100

    Mac moved to non-deprecated methods, huray!

--- mac.py
+++ mac.py
@@ -1,19 +1,24 @@
 # -*- coding: iso-8859-1 -*-
-from Quartz import CGPostMouseEvent, CGWarpMouseCursorPosition, CGDisplayPixelsHigh, CGDisplayPixelsWide
+from Quartz import *
 from AppKit import NSEvent
 from pymouse import PyMouseMeta
 
+pressID = [None, kCGEventLeftMouseDown, kCGEventRightMouseDown, kCGEventOtherMouseDown]
+releaseID = [None, kCGEventLeftMouseUp, kCGEventRightMouseUp, kCGEventOtherMouseUp]
+
 class PyMouse(PyMouseMeta):
     def press(self, x, y, button = 1):
-        button_list = [0, 0, 0]
-        button_list[button - 1] = 1
-        CGPostMouseEvent((x, y), 1, 3, *button_list)
+        event = CGEventCreateMouseEvent(None, pressID[button], (x, y), button - 1)
+	CGEventPost(kCGHIDEventTap, event)
 
     def release(self, x, y, button = 1):
-        CGPostMouseEvent((x, y), 1, 3, 0, 0, 0)
+	event = CGEventCreateMouseEvent(None, releaseID[button], (x, y), button - 1)
+	CGEventPost(kCGHIDEventTap, event)
 
     def move(self, x, y):
-        CGWarpMouseCursorPosition((float(x), float(y)))
+	move = CGEventCreateMouseEvent(None, kCGEventMouseMoved, (x, y), 0)
+	CGEventPost(kCGHIDEventTap, move)
+        
 
     def position(self):
         loc = NSEvent.mouseLocation()
