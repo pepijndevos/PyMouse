4365696f55031c9b37ee9de05ff6a8fcf2144546
Pepijn de Vos <pepijndevos@gmail.com>
2010-04-13 16:23:32 +0200

    Adding copyright notice

--- pymouse.py
+++ pymouse.py
@@ -1,5 +1,19 @@
 # -*- coding: iso-8859-1 -*-
 
+#   Copyright 2010 Pepijn de Vos
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 """The goal of PyMouse is to have a cross-platform way to control the mouse.
 PyMouse should work on Windows, Mac and any Unix that has xlib.
 
